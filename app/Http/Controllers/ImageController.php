e3adf2f50e415fa9f2b36dd07557ca7f0cfe4495
Alex <a_hutchings@hotmail.co.uk>
2020-08-04 22:20:25 +0000

    image controller comments added

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -13,15 +13,23 @@ class ImageController extends Controller
     {
 
         $image = $request->filename;
+
+        // beggining is removed from base64 string
         $image = str_replace('data:model/stl;base64,', '', $image);
+
+        // spaces are replaced with '+'
         $image = str_replace(' ', '+', $image);
+
+        // a name is randomly assigned
         $imageName = Str::random(10) . '.stl';
 
+        // the image is saved with it's image name for later retrieval
         $newimage = Image::create([
             'title' => $request->title,
             'filename' => $imageName
         ]);
 
+        // it is saved in storage/files
         Storage::disk('files')->put($imageName, base64_decode($image));
 
         return $newimage;
